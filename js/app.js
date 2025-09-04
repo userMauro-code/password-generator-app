06a82e4ba1236882764ce7b84e40bcbb8be22ec5
userMauro-code <m88642336@gmail.com>
2025-09-03 23:24:41 -0300

    commit js boxes

--- js/app.js
+++ js/app.js
@@ -6,8 +6,14 @@ const checkBoxes = document.querySelectorAll(".container_checkbox input[type='ch
 const textIndicator = document.getElementById("textIndicator");
 const barColor = document.querySelectorAll(".bars bar");
 const btnGenerate = document.getElementById("generate");
+// --------bloquesPassword-----------------------
+let bloqueUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let bloqueLowerCase = "abcdefghijklmnopqrstuvwxyz";
+let bloqueNumber = "0123456789";
+let bloqueSymbol = "!¡&%$#@*";
 
 
+// ---------input&span-----------------
 function updateRange() {
     const val = inputRange.value;
     rangeValue.textContent = val
@@ -16,3 +22,24 @@ function updateRange() {
 inputRange.addEventListener('input', updateRange);
 
 updateRange();
+
+// ----------checkboxs-----------
+
+
+checkBoxes.forEach(function(boxes) {
+    let inSelect = "";
+    
+   if (boxes.checked) {
+        if (boxes.id === "upperCase") {
+            inSelect + bloqueUpperCase;
+        }else if(boxes.id === "lowerCase"){
+            inSelect + bloqueLowerCase;
+        }else if(boxes.id === "numbers"){
+            inSelect + bloqueNumber;
+        }else if(boxes.id === "symbols"){
+            inSelect + symbols;
+        }
+   }
+   console.log(inSelect)
+});
+
